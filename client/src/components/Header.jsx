3cab224d1b2ce5fbd9031ff251146c327ae78947
FSD9-Wave <loomaflex44@gmail.com>
2025-08-21 11:02:34 +0700

    feat: integrate Tailwind CSS and PostCSS, restructure App component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="flex justify-center items-center h-10 mt-20">
+      <div className="text-8xl text-center text-blue-400">เที่ยวไหนดี</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+function Navigation() {
+  return (
+    <div className="flex justify-start items-center h-10 w-[60vw] mx-auto mt-20">
+        <div className="text-3xl text-start">ค้นหาที่เที่ยว</div>
+    </div>
+  );
+}
+
+export default Navigation;
